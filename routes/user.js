633b1f85909d6f031118c4110202f245ec661522
gongcj@yeastar.com <8627414+gong-chenjun@user.noreply.gitee.com>
2024-04-24 20:56:18 +0800

    feat(user): 新增删除、创建、查询所有用户接口

--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,22 @@ var User = require("../model/user");
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Types;
 
-// GET /users
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
-
-// POST /users
-router.post("/", function (req, res, next) {
-  res.send("create a user");
+// 获取所有用户信息
+router.get("/user", (req, res) => {
+  User.find({})
+    .then((data) => {
+      res.json({
+        status: 200,
+        msg: "查询成功",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        status: 404,
+        msg: err.message,
+      });
+    });
 });
 
 // 根据userId获取用户信息
@@ -39,6 +47,44 @@ router.get("/user/:userId", (req, res) => {
     });
 });
 
+// 新增用户
+router.post("/user", (req, res) => {
+  const { name = "", email, password, userGroup } = req.body;
+  // 查询是否存在相同email
+  User.findOne({ email }).then((existingUser) => {
+    // 存在-->无法注册
+    if (existingUser) {
+      return res.json({
+        status: 409,
+        msg: "该邮箱已被注册",
+      });
+    } else {
+      // 不存在-->创建新用户
+      const newUser = new User({
+        name,
+        email,
+        password,
+        userGroup,
+      });
+
+      return newUser
+        .save()
+        .then(() => {
+          res.json({
+            status: 200,
+            msg: "用户创建成功",
+          });
+        })
+        .catch((err) => {
+          res.json({
+            status: 500,
+            msg: err.message,
+          });
+        });
+    }
+  });
+});
+
 // 根据userId修改用户信息
 router.put("/user/:userId", (req, res) => {
   const userId = req.params.userId;
@@ -59,10 +105,28 @@ router.put("/user/:userId", (req, res) => {
     });
 });
 
-// DELETE /users/:id
-router.delete("/:id", function (req, res, next) {
-  var userId = req.params.id;
-  res.send("delete user with id: " + userId);
+// 根据userId删除用户信息
+router.delete("/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const objectId = new ObjectId(userId);
+  try {
+    const deletedUser = User.findByIdAndDelete(objectId);
+    if (!deletedUser) {
+      return res.json({
+        status: 404,
+        message: "未找到对应用户",
+      });
+    }
+    return res.json({
+      status: 200,
+      message: "成功删除用户",
+    });
+  } catch (error) {
+    return res.json({
+      status: 500,
+      message: "服务器错误",
+    });
+  }
 });
 
 module.exports = router;
